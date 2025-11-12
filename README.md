c07dec789ad7c6ecac9306b52e91d717d20cd5d3
Jesus-Cabrera-Avila <24308060610607@cetis61.edu.mx>
2025-11-12 10:12:21 -0500

    asdfgh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pokeapi
+pip freeze > requirements.txt
+
+pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
-API = "https://pokeapi.co/api/v2/pokemon/"
 app = Flask(__name__)
 app.secret_key = 'Mega_charizard_X'
-
+API = "https://pokeapi.co/api/v2/pokemon/"
 @app.route('/')
-def base():
-    return render_template('base.html')
-
-@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -20,11 +15,27 @@ if not pokemon_name:
     flash('por favor ingresa un nombre de pokemon', 'error')
     return redirect(url_for('index'))
 
-resp = requests.get(f"{API}{pokemon_name}")
-
-if resp.status_code == 200:
+try:
+    resp = requests.get(f"{API}{pokemon_name}")
+    if resp.status_code == 200:
     pokemon_data = resp.json()
-    return render_template('pokemon.html', pokemon = pokemon_)
+
+        poke_info = {
+            'name': pokemon_data['name'].tittle(),
+            'id': pokemon_data['id'],
+            'height': pokemon_data['height'] / 10, 
+            'weight': pokemon_data['weight'] / 10,
+            'image': pokemon_data['sprites']['front_default'],
+            'types': [t['ability']['name'].title() for t in pokemon_data['types']],
+            'abilites': [a['ability']['name'].title() for a in pokemon_data['abilites']],
+        }
+        return render_template('pokemon2.html', pokemon = poke_info)
+    else:
+        flash(f'pokemon "{pokemon_name}" no fue encontrado', 'error')
+        return redirect(url_for('index'))
+except requests.exceptions.RequestException as e:
+    flash('Error al buscar el pokemon', 'error')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
